59edaa72111e5390f8707a4aeef655b9dd45df50
Dzulhilmi <=>
2023-10-28 09:52:54 +0700

    add: messages api

---
+++ lib/current-profile-pages.ts
@@ -0,0 +1,21 @@
+import { auth } from "@clerk/nextjs";
+
+import { db } from "@/lib/db";
+import { NextApiRequest } from "next";
+import { getAuth } from "@clerk/nextjs/server";
+
+export const currentProfilePages = async (req: NextApiRequest) => {
+  const { userId } = getAuth(req);
+
+  if (!userId) {
+    return null;
+  }
+
+  const profile = await db.profile.findUnique({
+    where: {
+      userId,
+    },
+  });
+
+  return profile;
+};
